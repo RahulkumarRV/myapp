01fb64850a8ffc141068a69cd4af1cc78f88cf85
rahul <rahulkumar665550@gmail.com>
2022-07-15 17:36:46 +0530

    typescript

--- App.tsx
+++ App.tsx
@@ -1,12 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator, NativeStackScreenProps} from "@react-navigation/native-stack";
+import { RootStackParamList } from './tyes';
+import { useState } from 'react';
+
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+const  HomeScreen: React.FC<HomeScreenProps> = (props) => {
+  const [keyInput, setKeyInput] = useState("");
+
+  async function handleApi (){
+      await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${keyInput}?api_key=DEMO_KEY`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        props.navigation.push("Profile", {data : data});
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  async function handleRandomApi(){
+    await fetch(` https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`)
+      .then((data) => data.json())
+      .then((data) => {
+        const datalist = data.near_earth_objects;
+        console.log(datalist.length);
+        
+        props.navigation.push("Profile", {data : datalist[Math.floor(Math.random() * datalist.length)]});
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <View>
+      <TextInput 
+      style={{ width: 200, height: 40, padding: 5, marginBottom: 5, backgroundColor: "lightgray"}}
+      placeholder='Enter Key'
+      value={keyInput}
+      onChangeText={setKeyInput}
+      ></TextInput>
+      <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap", height: 50}}>
+        <Button title='Submit' onPress={() => { handleApi()}}/>
+        <Button title='Random' onPress={() => handleRandomApi()}/>
+      </View>
+      </View>
+    </View>
+  );
+}
+
+type ProfileScreenProps = NativeStackScreenProps<RootStackParamList, "Profile">;
+
+const  ProfileScreen: React.FC<ProfileScreenProps> = (props) => {
+
+  
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Button title='Home' onPress={() => props.navigation.push("Home")}></Button>
+      <Text>{JSON.stringify( props.route.params.data)}</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name='Profile' component={ProfileScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
